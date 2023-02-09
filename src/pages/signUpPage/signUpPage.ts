7d4541f05bdfbc23a820bf04641adcc075ed530a
Murad <mailprostoimail@gmail.com>
2023-02-09 16:47:45 +0300

    fix: fix bugs

--- src/pages/signUpPage/signUpPage.ts
+++ src/pages/signUpPage/signUpPage.ts
@@ -45,6 +45,8 @@ class SignUpPage {
     this.appendTo(signInContent, signIn);
     const signUpFormBodyWrapper = createHtmlElement('sign-up-form__body-wrapper', 'div');
     const signUpFormBody = createHtmlElement('sign-up-form__body', 'form');
+    let [loginIsValid, firstLastNameIsValid, userNameIsValid, passwordIsValid] = [false, false, false, false];
+
     const signUpFormBodyLoginWrapper = createHtmlElement(['signUp-form-input-wrapper'], 'div');
     const signUpFormBodyLogin = createHtmlElement(
       ['signUp-form__mobile-or-email', 'signUp-form__input'],
@@ -59,7 +61,9 @@ class SignUpPage {
       if (target.value.search(regex) === -1) {
         target.parentElement?.classList.add('signUp-form__input--invalid');
         target.parentElement?.classList.remove('signUp-form__input--valid');
+        loginIsValid = false;
       } else {
+        loginIsValid = true;
         target.parentElement?.classList.add('signUp-form__input--valid');
         target.parentElement?.classList.remove('signUp-form__input--invalid');
       }
@@ -67,6 +71,7 @@ class SignUpPage {
       if (String(target.value.length) === '0') {
         target.parentElement?.classList.remove('signUp-form__input--invalid');
         target.parentElement?.classList.remove('signUp-form__input--valid');
+        loginIsValid = false;
       }
     };
 
@@ -82,11 +87,14 @@ class SignUpPage {
       if (target.value.length > 3) {
         target.parentElement?.classList.remove('signUp-form__input--invalid');
         target.parentElement?.classList.add('signUp-form__input--valid');
+        firstLastNameIsValid = true;
       } else {
         target.parentElement?.classList.add('signUp-form__input--invalid');
         target.parentElement?.classList.remove('signUp-form__input--valid');
+        firstLastNameIsValid = false;
       }
       if (target.value === '') {
+        firstLastNameIsValid = false;
         target.parentElement?.classList.remove('signUp-form__input--invalid');
         target.parentElement?.classList.remove('signUp-form__input--valid');
       }
@@ -105,13 +113,16 @@ class SignUpPage {
       if (target.value.length > 3) {
         target.parentElement?.classList.remove('signUp-form__input--invalid');
         target.parentElement?.classList.add('signUp-form__input--valid');
+        userNameIsValid = true;
       } else {
         target.parentElement?.classList.add('signUp-form__input--invalid');
         target.parentElement?.classList.remove('signUp-form__input--valid');
+        userNameIsValid = false;
       }
       if (target.value === '') {
         target.parentElement?.classList.remove('signUp-form__input--invalid');
         target.parentElement?.classList.remove('signUp-form__input--valid');
+        userNameIsValid = false;
       }
     };
 
@@ -120,18 +131,17 @@ class SignUpPage {
       ['signUp-form__password', 'signUp-form__input'],
       'input'
     ) as HTMLInputElement;
-    const signUpFormBodyPasswordRequirements = createHtmlElement(
-      ['signUp-form__password-requirements', 'signUp-form__input'],
-      'div'
-    );
-    signUpFormBodyPasswordRequirements.innerHTML = `
-    <ul style="margin: 0; padding: 0 0 0 20px">
-      <li style="list-style: none; font-size: 10px;">Требования к паролю:</li>
-
-      <li>Длина пароля от 6 до 20 символов.</li>
-      <li>Одна прописная и одна строчная буква.</li>
-    </ul>
-    `;
+    // const signUpFormBodyPasswordRequirements = createHtmlElement(
+    //   ['signUp-form__password-requirements', 'signUp-form__input'],
+    //   'div'
+    // );
+    // signUpFormBodyPasswordRequirements.innerHTML = `
+    // <ul class="password-requirements" style="margin: 0; padding: 0 0 0 20px">
+    //   <li style="list-style: none; font-size: 10px;">Требования к паролю:</li>
+    //   <li>Длина пароля от 6 до 20 символов.</li>
+    //   <li>Одна прописная и одна строчная буква.</li>
+    // </ul>
+    // `;
     signUpFormBodyPassword.placeholder = 'Пароль';
     signUpFormBodyPassword.type = 'password';
     signUpFormBodyPassword.pattern = '((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{3,20})';
@@ -139,10 +149,10 @@ class SignUpPage {
     signUpFormBodyPassword.oninput = (e) => {
       const target = e.target as HTMLInputElement;
       const showUnshowPassword = createHtmlElement('show-unshow-password', 'div', 'Показать');
-      if (!(target.nextElementSibling?.className === 'show-unshow-password')) {
+      if (!(target.nextElementSibling?.nextElementSibling?.className === 'show-unshow-password')) {
         target.parentElement?.append(showUnshowPassword);
       }
-      showUnshowPassword.onclick = (e) => {
+      showUnshowPassword.onclick = () => {
         if (target.type === 'password') {
           target.type = 'text';
           showUnshowPassword.textContent = 'Скрыть';
@@ -155,19 +165,53 @@ class SignUpPage {
       showUnshowPassword.style.top = '9px';
       showUnshowPassword.style.right = '40px';
       showUnshowPassword.style.fontSize = '14px';
-      showUnshowPassword.style.fontWeight = 'bold';
+      showUnshowPassword.style.fontWeight = '400';
+      showUnshowPassword.style.cursor = 'pointer';
+      const regexEnLower = new RegExp('[a-z]', 'gm');
+      const regexEnUpper = new RegExp('[A-Z]', 'gm');
       const pattern = target.pattern;
+      const passwordRequirements = document.querySelector('.password-requirements') as HTMLUListElement;
+      if (target.value.length >= 6 && target.value.length <= 20) {
+        const passwordRequirements = document.querySelector('.password-requirements') as HTMLUListElement;
+        passwordRequirements.firstElementChild?.nextElementSibling?.classList.add('isRight');
+        passwordRequirements.firstElementChild?.nextElementSibling?.classList.remove('isNotRight');
+      } else {
+        passwordRequirements.firstElementChild?.nextElementSibling?.classList.add('isNotRight');
+        passwordRequirements.firstElementChild?.nextElementSibling?.classList.remove('isRight');
+      }
+      if (target.value.search(regexEnLower) !== -1 || target.value.search(regexEnUpper) !== -1) {
+        passwordRequirements.firstElementChild?.nextElementSibling?.nextElementSibling?.classList.add('isRight');
+        passwordRequirements.firstElementChild?.nextElementSibling?.nextElementSibling?.classList.remove('isNotRight');
+      } else {
+        passwordRequirements.firstElementChild?.nextElementSibling?.nextElementSibling?.classList.add('isNotRight');
+        passwordRequirements.firstElementChild?.nextElementSibling?.nextElementSibling?.classList.remove('isRight');
+      }
+      if (
+        target.value.length >= 6 &&
+        target.value.length <= 20 &&
+        target.value.search(regexEnLower) !== -1 &&
+        target.value.search(regexEnUpper) !== -1
+      ) {
+        passwordIsValid = true;
+      }
+      target.parentElement?.classList.add('signUp-form__input--invalid');
+      target.parentElement?.classList.remove('signUp-form__input--valid');
       if (target.value.search(pattern) === -1) {
-        target.parentElement?.classList.add('signUp-form__input--invalid');
-        target.parentElement?.classList.remove('signUp-form__input--valid');
+        console.log('.');
       } else {
         target.parentElement?.classList.remove('signUp-form__input--invalid');
         target.parentElement?.classList.add('signUp-form__input--valid');
+        passwordRequirements.firstElementChild?.nextElementSibling?.nextElementSibling?.classList.add('isRight');
+        passwordRequirements.firstElementChild?.nextElementSibling?.classList.remove('isNotRight');
       }
       if (target.value === '') {
         target.parentElement?.classList.remove('signUp-form__input--invalid');
         target.parentElement?.classList.remove('signUp-form__input--valid');
-        target.nextElementSibling?.remove();
+        target.nextElementSibling?.nextElementSibling?.remove();
+        passwordIsValid = false;
+        passwordRequirements.firstElementChild?.nextElementSibling?.classList.remove('isNotRight');
+        passwordRequirements.firstElementChild?.nextElementSibling?.nextElementSibling?.classList.remove('isNotRight');
+        passwordRequirements.firstElementChild?.nextElementSibling?.nextElementSibling?.classList.remove('isRight');
       }
     };
 
@@ -180,7 +224,14 @@ class SignUpPage {
       'button',
       'Далее'
     ) as HTMLInputElement;
-
+    signUpFormBodyButtonSubmit.disabled = true;
+    const attention = createHtmlElement('attention', 'div');
+    signUpFormBody.oninput = () => {
+      if (loginIsValid && firstLastNameIsValid && userNameIsValid && passwordIsValid) {
+        signUpFormBodyButtonSubmit.disabled = false;
+        signUpFormBodyButtonSubmit.classList.add('signUp-form__submit--valid');
+      }
+    };
     const signUpFormBodyLineWrapper = createHtmlElement('signUp-form__line-wrapper', 'div');
     const signUpFormBodyLineLeft = createHtmlElement('signUp-form__line-left', 'div');
     const signUpFormBodyLineText = createHtmlElement('signUp-form__line-text', 'div', 'ИЛИ');
@@ -198,10 +249,11 @@ class SignUpPage {
     this.appendTo(signUpFormBodyUserName, signUpFormBodyUserNameWrapper);
     this.appendTo(signUpFormBodyUserNameWrapper, signUpFormBody);
     this.appendTo(signUpFormBodyPassword, signUpFormBodyPasswordWrapper);
-    this.appendTo(signUpFormBodyPasswordRequirements, signUpFormBodyPasswordWrapper);
+    // this.appendTo(signUpFormBodyPasswordRequirements, signUpFormBodyPasswordWrapper);
     this.appendTo(signUpFormBodyPasswordWrapper, signUpFormBody);
     this.appendTo(signUpFormBodyPolicy, signUpFormBody);
     this.appendTo(signUpFormBodyButtonSubmit, signUpFormBody);
+    this.appendTo(attention, signUpFormBody);
     this.appendTo(signUpFormBody, signUpFormBodyWrapper);
     this.appendTo(signUpFormBodyWrapper, signUp);
 
