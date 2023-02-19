1a7e92ce6870658c2a7a793ba8358533a3ca5548
Murad <mailprostoimail@gmail.com>
2023-02-19 22:39:19 +0300

    feat: add user post component

--- src/components/userPublications/userPublications.ts
+++ src/components/userPublications/userPublications.ts
@@ -1,7 +1,10 @@
 import { createHtmlElement } from '../../utils/createHtmlElement';
 const publicationsIcon = require('../../assets/icons/publications-icon.svg');
 const savedIcon = require('../../assets/icons/saved-icon.svg');
-
+const likedIcon = require('../../assets/icons/liked-icon.svg');
+const commentIcon = require('../../assets/icons/comment-icon.svg');
+const smileIcon = require('../../assets/icons/smile-icon.svg');
+const dotsIcon = require('../../assets/icons/dots-icon.svg');
 export default class UserPublicationsComponent {
   wrapper: HTMLElement;
 
@@ -11,6 +14,75 @@ export default class UserPublicationsComponent {
 
   appendTo(elem: HTMLElement, toElem: HTMLElement) {
     toElem.append(elem);
+  }
+  appendAll(elements: HTMLElement[], to: HTMLElement) {
+    elements.forEach((elem) => {
+      to.append(elem);
+    });
+  }
+
+  getUserPostComponent() {
+    const wrapper = createHtmlElement('user-post-wrapper', 'div');
+    wrapper.addEventListener('click', (e) => {
+      const target = e.target as HTMLElement;
+      if (!target.offsetParent?.className.includes('user-post-wrapper')) {
+        wrapper.remove();
+      }
+    });
+    const innerWrapper = createHtmlElement('user-post-wrapper-inner', 'div');
+    wrapper.append(innerWrapper);
+
+    const userPostImageWrapper = createHtmlElement('user-post-image-wrapper', 'div');
+    const userPostImage = createHtmlElement('user-post-image', 'img') as HTMLImageElement;
+    userPostImage.src = 'https://via.placeholder.com/500';
+    userPostImage.style.backgroundSize = 'cover';
+    userPostImageWrapper.append(userPostImage);
+    innerWrapper.append(userPostImageWrapper);
+
+    const userPostContentWrapper = createHtmlElement('user-post-content-wrapper', 'div');
+    const userPostContentHeader = createHtmlElement('user-post-content-header', 'div');
+    const userPostContentHeaderIcon = createHtmlElement('user-post-content-header__icon', 'img') as HTMLImageElement;
+    userPostContentHeaderIcon.src = 'https://via.placeholder.com/24';
+    const userPostContentHeaderUserName = createHtmlElement('user-post-content-header__username', 'div', 'username');
+    const userPostSettingsIcon = createHtmlElement('user-post-settings-icon', 'img') as HTMLImageElement;
+    userPostSettingsIcon.src = dotsIcon;
+    this.appendAll(
+      [userPostContentHeaderIcon, userPostContentHeaderUserName, userPostSettingsIcon],
+      userPostContentHeader
+    );
+
+    const userPostContentText = createHtmlElement(
+      'user-post-content-text',
+      'div',
+      ' fsdfsdfdloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem'
+    );
+    userPostContentText.insertAdjacentHTML('afterbegin', '<span class="post-username">username</span>');
+    const userPostContentReactions = createHtmlElement('user-post-content-reactions', 'div');
+    const userPostContentLiked = createHtmlElement('user-post-content-liked', 'img') as HTMLImageElement;
+    const userPostContentComments = createHtmlElement('user-post-content-comments', 'img') as HTMLImageElement;
+    userPostContentLiked.src = likedIcon;
+    userPostContentComments.src = commentIcon;
+    this.appendAll([userPostContentLiked, userPostContentComments], userPostContentReactions);
+    this.appendAll([userPostContentHeader, userPostContentText, userPostContentReactions], userPostContentWrapper);
+
+    const userCommentsWrapper = createHtmlElement('user-post-comments-wrapper', 'div');
+    const userCommentsSmilesWrapper = createHtmlElement('user-post-comments-smiles-wrapper', 'div');
+    const userCommentsSmiles = createHtmlElement('user-post-comments-smiles', 'img') as HTMLImageElement;
+    userCommentsSmiles.src = smileIcon;
+    userCommentsSmilesWrapper.append(userCommentsSmiles);
+    const userCommentsTextInput = createHtmlElement('user-post-comments-input', 'input') as HTMLInputElement;
+    userCommentsTextInput.placeholder = 'Добавьте комментарий';
+    const userCommentsButtonSend = createHtmlElement(
+      'user-post-comments-button',
+      'button',
+      'Опубликовать'
+    ) as HTMLButtonElement;
+    userCommentsButtonSend.disabled = true;
+    this.appendAll([userCommentsSmiles, userCommentsTextInput, userCommentsButtonSend], userCommentsWrapper);
+
+    userPostContentWrapper.append(userCommentsWrapper);
+    innerWrapper.append(userPostContentWrapper);
+    return wrapper;
   }
 
   draw() {
@@ -54,6 +126,14 @@ export default class UserPublicationsComponent {
     this.appendTo(UserPublications, this.wrapper);
 
     const UserPublicationsItemsWrapper = createHtmlElement('user-publications-items-wrapper', 'div');
+    UserPublicationsItemsWrapper.addEventListener('click', (e) => {
+      const target = e.target as HTMLElement;
+
+      if (target.className.includes('user-publications-item')) {
+        const component = this.getUserPostComponent(/*id, postId*/)
+        document.body.prepend(component);
+      }
+    });
     const UserPublicationsItems = createHtmlElement('user-publications-items', 'div');
     const UserPublicationsItem = createHtmlElement('user-publications-item', 'div');
     this.appendTo(UserPublicationsItems, UserPublicationsItemsWrapper);
