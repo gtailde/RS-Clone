bced0a4214e456ea60c1e284c4c293a9b0ba3194
Murad <mailprostoimail@gmail.com>
2023-02-27 20:52:20 +0300

    fix: bugs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import implementsUserProfilePage from "./pages/userProfilePage/userProfilePage";
 import implementsAuthorizationPage from "./pages/authorizationPage/authorizationPage";
 import implementsSignUpPage from "./pages/signUpPage/signUpPage";
 
-if (window.localStorage.getItem('userDataAccess')) {
+if (window.localStorage.getItem('userDataAccess')?.includes('id')) {
   const data = JSON.parse(window.localStorage.getItem('userDataAccess')!);
   const startUserProfilePage = new implementsUserProfilePage(data.id);
   startUserProfilePage.draw();
